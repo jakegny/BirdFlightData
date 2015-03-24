485bf018682650520138ffad4ca2c1f6543ade43
Jake Nylund <jakegny@gmail.com>
2015-03-24 07:18:39 -0700

    World Map added
    World map added. Still need to scale properly

--- js/bfd.js
+++ js/bfd.js
@@ -2,7 +2,7 @@ function draw(geo_data) {
   "use strict";
   var margin = 75,
       width = 1050 - margin,
-      height = 600 - margin;
+      height = 610 - margin;
 
   var svg = d3.select("#map")
       .append("svg")
@@ -10,4 +10,20 @@ function draw(geo_data) {
       .attr("height", height + margin) 
       .append('g')
       .attr('class', 'USmap');
+
+  var projection = d3.geo.mercator()
+                         .scale(150)
+                         .translate( [width / 2, height / 1.5]);
+
+  var path = d3.geo.path().projection(projection);
+
+  var map = svg.selectAll('path')
+               .data(geo_data.features)
+               .enter()
+               .append('path')
+               .attr('d', path)
+               .style('fill', 'lightBlue')
+               .style('stroke', 'black')
+               .style('stroke-width', 0.5);
+
 };

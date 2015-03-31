42294a6ecf3d18873a805dc34f38b0d79e12472b
Jake Nylund <jakegny@gmail.com>
2015-03-30 22:11:48 -0700

    Leaflet North America Sample
    Leaflet sample added for North America specifically.

--- js/bfd.js
+++ js/bfd.js
@@ -12,8 +12,8 @@ function draw(geo_data) {
       .attr('class', 'USmap');
 
   var projection = d3.geo.mercator()
-                         .scale(150)
-                         .translate( [width / 2, height / 1.5]);
+                         .scale(140)
+                         .translate( [width / 2, height / 1.2]);
 
   var path = d3.geo.path().projection(projection);
 
@@ -26,4 +26,43 @@ function draw(geo_data) {
                .style('stroke', 'black')
                .style('stroke-width', 0.5);
 
+  function plot_points(data) {
+    //draw circles logic
+    debugger;
+    var nested = d3.nest()
+                   .key(function(d) {
+                      debugger;
+                      return d['date'].getUTCFullYear();
+                   })
+                   .rollup(function(leaves) {
+                      debugger;
+                      var total = d3.sum(leaves, function(d){
+                        return d['attendance'];
+                      });
+                      var coords = leaves.map(function(d){
+                        return projection([+d.long, +d.lat]);
+                      });
+                      var center_x = d3.mean(coords, function(d){
+                        return d[0];
+                      });
+
+                   })
+                   .entries(data);
+  };
+
+  var format = d3.time.format("%d-%m-%Y (%H:%M h)");
+
+  d3.tsv("world_cup_geo.tsv", function(d){
+    d['attendance'] = +d['attendance'];
+    d['date'] = format.parse(d['date']);
+    return d;
+  }, plot_points);
+
+
+
+
+
+
+
 };
+
